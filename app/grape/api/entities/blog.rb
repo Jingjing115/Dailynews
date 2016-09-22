cf48b0afc1a369348f204107c248304645e57f1b
zhuguo <phantomdev@zhuguodeMBP.lan>
2016-09-22 16:44:37 +0800

    fix

--- app/grape/api/entities/blog.rb
+++ app/grape/api/entities/blog.rb
@@ -1,7 +1,7 @@
 module API
   module Entities
     class Blog < Grape::Entity
-      format_with(:iso_timestamp) { |dt| dt.try :utf8 }
+      format_with(:iso_timestamp) { |dt| dt.try :iso8601 }
       expose :id, :documentation => {
         type: "Integer",
         desc: "blogID",
